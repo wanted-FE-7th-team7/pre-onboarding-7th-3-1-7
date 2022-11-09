15c783215724b3cd030ba81b6abb0035c7a09ba7
nyoung <75849590+nyoung113@users.noreply.github.com>
2022-11-09 20:57:06 +0900

    Style: global style 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
-  return <div className="App">여기에 앱을 작성해주세요.</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <div className="App">여기에 앱을 작성해주세요.</div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/styles/GlobalStyles.tsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+import { reset } from 'styled-reset';
+
+const GlobalStyles = createGlobalStyle`
+ ${reset}
+html {
+  font-size: 10px;
+  box-sizing : border-box;
+}
+`;
+export default GlobalStyles;
