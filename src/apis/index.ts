3fbbf88e667d7f09ab7da7d62109f62ec4d9ec7f
nyoung <75849590+nyoung113@users.noreply.github.com>
2022-11-10 11:40:11 +0900

    Feat: api 호출 구현

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,2 +1,18 @@
-// 해당 파일은 삭제하셔도 됩니다.
-export {};
+import axios from 'axios';
+import { Sick } from '../interfaces';
+
+const API_ENDPOINT = 'http://localhost:4000';
+
+export const getSearchResult = async (query: string) => {
+  try {
+    console.log('api 호출');
+    const res = await axios.get<Sick[]>(`${API_ENDPOINT}/sick`, {
+      params: { q: query },
+    });
+    console.log(res.data);
+    return res.data;
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
+};
