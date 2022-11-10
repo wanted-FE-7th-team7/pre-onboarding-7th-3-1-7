089f563b7a43fa29efb9157a36ac65ffa7ad78ee
Chaedie <chaedong.im.dev@gmail.com>
2022-11-10 12:49:20 +0900

    Feat: 캐싱 기능 구현

--- src/apis/Sicks.service.ts
+++ src/apis/Sicks.service.ts
@@ -1,11 +1,21 @@
 import { AxiosResponse } from 'axios';
 import { http, URLS } from './api';
-import { Sick } from '../types';
+import { Cache, Sick } from '../types';
+
+const cache: Cache = {};
 
 export const getSicksQuery = async (query: string) => {
+  if (cache[query]) {
+    console.info('use Cache');
+    return cache[query];
+  }
+
   console.info('calling api');
   const { data }: AxiosResponse<Sick[]> = await http.get(URLS.SICK, {
     params: { sickNm_like: query },
   });
+
+  cache[query] = data;
+
   return data;
 };
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,7 @@ export interface Sick {
   sickCd: string;
   sickNm: string;
 }
+
+export interface Cache {
+  [index: string]: Sick[];
+}
